cf4b73c1ebea044e802104b93a8e88825ebe4135
Oséias Costa <oseiasc2@gmail.com>
2022-10-31 18:36:12 +0000

    Mobile Updates started

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,20 +1,26 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
 import Logo from "./icons/Logo";
 
 const Header = () => {
-    return(
-        <header>
-            <Logo />
-            <nav>
-                <ul className="Header__Menu">
-                    <li><a href="#HowWork">Como funciona</a></li>
-                    <li><a href="#OurServices">Serviços</a></li>
-                    <li><a href="#Clients">Clientes</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header>
+      <Logo className="Header__Logo" />
+      <nav>
+        <ul className="Header__Menu">
+          <li>
+            <a href="#HowWork">Como funciona</a>
+          </li>
+          <li>
+            <a href="#OurServices">Serviços</a>
+          </li>
+          <li>
+            <a href="#Clients">Clientes</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
