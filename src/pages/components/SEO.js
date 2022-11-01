2c7aae51d86b9a3b87a3dbb5b8a3d40d3b57848c
Oséias Costa <oseiasc2@gmail.com>
2022-11-01 20:53:54 +0000

    SEO created Meta Tags

---
+++ src/pages/components/SEO.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const SEO = () => {
+  return (
+    <>
+      <title>Mad Digital </title>
+      <meta name="title" content="Mad Digital " />
+      <meta
+        name="description"
+        content="A forma mais eficaz de escalar seu negócio com Marketing Digital. Com o método Mad cresça seus resultados com Tráfego Pago na captação de novos clientes."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.madcompany.com.br/" />
+      <meta property="og:title" content="Mad Digital " />
+      <meta
+        property="og:description"
+        content="A forma mais eficaz de escalar seu negócio com Marketing Digital. Com o método Mad cresça seus resultados com Tráfego Pago na captação de novos clientes."
+      />
+      <meta
+        property="og:image"
+        content="https://valioze.com.br/clientes/Mad-Digital.png"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://www.madcompany.com.br/" />
+      <meta property="twitter:title" content="Mad Digital " />
+      <meta
+        property="twitter:description"
+        content="A forma mais eficaz de escalar seu negócio com Marketing Digital. Com o método Mad cresça seus resultados com Tráfego Pago na captação de novos clientes."
+      />
+      <meta
+        property="twitter:image"
+        content="https://valioze.com.br/clientes/Mad-Digital.png"
+      />
+    </>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import React from "react"
-import Clients from "./components/Clients"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import HowWork from "./components/HowWork"
-import OurServices from "./components/OurServices"
-import Results from "./components/Results"
-import Start from "./components/Start"
-import Whats from "./components/Whats"
-import './index.css'
-
+import React from "react";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HowWork from "./components/HowWork";
+import OurServices from "./components/OurServices";
+import Results from "./components/Results";
+import Start from "./components/Start";
+import Whats from "./components/Whats";
+import "./index.css";
+import { SEO } from "./components/SEO";
 
 const IndexPage = () => {
   return (
@@ -22,9 +22,11 @@ const IndexPage = () => {
       <Footer />
       <Whats />
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => {
+  return <SEO />;
+};
