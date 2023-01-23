05a7b6abfdf2b02b6f073e5ab128612b5a4b876d
Oseias Costa <oseiascosta@iMac-de-Oseias.local>
2023-01-23 12:42:28 -0300

    Form with states

--- src/pages/campanhas/Form.js
+++ src/pages/campanhas/Form.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import './Form.css'
+import  axios  from "axios";
 
 const Form = () => {
+const [cadastro, setCadatro] = useState({
+    Nome: '',
+    Email: '',
+    Telefone: '',
+    Instagram: '',
+    Cargo: '',
+    campanha: 'clinica'
+})
+
+const Send = () => {
+    axios.post('https://script.google.com/macros/s/AKfycbyU1lbwdubafmshznnsZoZaAyLKdTRisPW0T65diIzHt5ja-X29sRbYbg6gNgVV3zt2NA/exec',
+             {
+            ...cadastro
+        })
+}
+
     return (
         <section className="formulario">
             <div>
@@ -9,12 +26,20 @@ const Form = () => {
                 <p>PREENCHA O FORMULÁRIO ABAIXO</p>
             </div>
             <div className="inputs">
-                <input type='text' placeholder="Nome e Sobrenome" /><br />
-                <input type='text' placeholder="Seu melhor e-mail" /><br />
-                <input type='text' placeholder="Seu número de WhatsApp com DDD" /><br />
-                <input type='text' placeholder="Qual é seu Instagram" /><br />
-                <input type='text' placeholder="Qual é seu cargo na empresa" /><br />
-                <button>Quero Receber o Contato da Mad Digital</button>
+                <input type='text' 
+                    value={cadastro.Nome} onChange={(e) => setCadatro({...cadastro, Nome: e.target.value})}  placeholder="Nome e Sobrenome" /><br />
+                <input type='text'  
+                    value={cadastro.Email} onChange={(e) => setCadatro({...cadastro, Email: e.target.value})}  placeholder="Seu melhor e-mail" /><br />
+                <input type='text'
+                 value={cadastro.Telefone} onChange={(e) => setCadatro({...cadastro, Telefone: e.target.value})}  
+                placeholder="Seu número de WhatsApp com DDD" /><br />
+                <input type='text' 
+                value={cadastro.Instagram} onChange={(e) => setCadatro({...cadastro, Instagram: e.target.value})}
+                placeholder="Qual é seu Instagram" /><br />
+                <input type='text' 
+                value={cadastro.Cargo} onChange={(e) => setCadatro({...cadastro, Cargo: e.target.value})}
+                placeholder="Qual é seu cargo na empresa" /><br />
+                <button onClick={Send}>Quero Receber o Contato da Mad Digital</button>
             </div>
         </section>
     )   
